25017cd5f1a879337d386c0d827f976dc11bb66e
gurkiratsinghofficial <gurkiratsinghofficial@gmail.com>
2019-07-12 15:55:36 +0530

    hello git

---
+++ linearsearch.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+int arr[]={1,2,3,4,5};
+int n=3;
+for(int i=0;i<5;i++)
+{
+if(arr[i]==n)
+{
+printf("element is present in array");
+}
+else
+{
+printf("element is not present");
+}
+}
+return 0;
+}"
