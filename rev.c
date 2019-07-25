321a234d4c0f5d9da87c7682e71093ba9479c3c7
gurkiratsinghofficial <gurkiratsinghofficial@gmail.com>
2019-07-25 19:07:29 +0530

    reverse of array using recursion

---
+++ rev.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+void revrec();
+int main()
+{
+   int size; 
+    scanf("%d",&size);
+    int arr[size];
+    for(int i=0;i<size;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    revrec(arr,size);
+   for(int i=0;i<size;i++)
+   {
+       printf("%d",arr[i]);
+   }
+   return 0; 
+}
+void revrec(int arr[],int size)
+{
+    int start=0;
+    int end=size-1;
+    int temp;
+    while(start<end)
+    {
+        temp=arr[start];
+        arr[start]=arr[end];
+        arr[end]=temp;
+        start++;
+        end--;
+    }
+}
+
